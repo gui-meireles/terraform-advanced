820dd9522bdae50da64dfcd08400eae1067c5d93
gui-meireles <guilhermemeirelesnascimento@gmail.com>
2024-07-21 16:52:31 -0300

    fix: Adicionando providers.tf dentro de main.tf

--- main.tf
+++ main.tf
@@ -1,3 +1,15 @@
+terraform {
+  required_version = ">=0.13.1"
+  required_providers {
+    aws = ">=3.54.0"
+    local = ">=2.1.0"
+  }
+}
+
+provider "aws" {
+  region = "us-east-1"
+}
+
 module "new-vpc" {
   source = "./modules/vpc"
 
